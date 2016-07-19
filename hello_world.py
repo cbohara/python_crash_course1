87c25175750e3d7d5c9ff337eac17f4a6810898e
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-19 10:42:01 -0700

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+message = "Hello Python world!"
+print(message)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+message = "Hello, " + first_name.title() + "!"
+print(message)
+
+print("Languages:\n\tJavascript\n\tRuby\n\tPython")
+
+favorite_language = 'python '
+favorite_language.rstrip()
+print(favorite_language)
---
+++ string.py
@@ -0,0 +1,18 @@
+name = "Charlie"
+print("Hello " + name + ", would you like to learn some python today?")
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+quote = 'Carl Rogers once said, "The curious paradox is that when I accept myself just as I am, then I can change."'
+print(quote)
+
+famous_person = 'Carl Rogers'
+message = famous_person + ' once said, "The curious paradox is that when I accept myself just as I am, then I can change."'
+print(message)
+
+name = "              Spacey Sally  "
+print(name)
+name = name.strip()
+print(name)
